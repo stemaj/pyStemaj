42088f5448502bb3920fd2ea2ad56d831d761241
Steffen Majaura <steffen.majaura@gmail.com>
2021-05-13 17:14:50 +0200

    bla

--- stringExtractor.py
+++ stringExtractor.py
@@ -5,12 +5,12 @@ def matchByRegex(daten: bytes, pattern: bytes) -> str:
     if match is not None and len(match.groups()) > 0:
         return match.group(1).decode('utf-8')
 
-def matchesByRegex2(daten: bytes, pattern: bytes) -> str:
+def matchesByRegex2(daten: bytes, pattern: bytes):
     match = search(pattern, daten)
     if match is not None and len(match.groups()) > 0:
         return [match.group(1).decode('utf-8'), match.group(2).decode('utf-8')]
 
-def matchesByRegex3(daten: bytes, pattern: bytes) -> str:
+def matchesByRegex3(daten: bytes, pattern: bytes):
     match = search(pattern, daten)
     if match is not None and len(match.groups()) > 0:
         return [match.group(1).decode('utf-8'), match.group(2).decode('utf-8'), match.group(3).decode('utf-8')]
