bb48d0dbbf979b9081fc13b5c7f5e95fa81905c8
Steffen Majaura <steffen.majaura@gmail.com>
2021-05-14 11:14:06 +0200

    extractInnerPartAndSplit

--- bytesExtractor.py
+++ bytesExtractor.py
@@ -5,3 +5,13 @@ def extractInnerPart(content: bytes, beginString: bytes, endString: bytes) -> by
         if len(splitt) > 1:
             return splitt[0]
     return b''
+
+def extractInnerPartAndSplit(content: bytes, beginString: bytes, endString: bytes, splitter: bytes):
+    splitt = content.split(beginString)
+    if len(splitt) > 1:
+        splitt = splitt[1].split(endString)
+        if len(splitt) > 1:
+            data = splitt[0].split(splitter)
+            data.pop()
+            return data
+    return b''
